7e9d9a4ea38c524ca08a626260ec6f49e5ed5d70
lemai134 <withuwe021@gmail.com>
2021-03-16 13:28:23 +0700

    Craete models

---
+++ eShopSolution.Data/Entities/Order.cs
@@ -0,0 +1,18 @@
+﻿using eShopSolution.Data.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Data.Entities
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public DateTime OrderDate { get; set; }
+        public int UserId { get; set; }
+        public string ShipName { get; set; }
+        public string ShipAddress { get; set; }
+        public decimal ShipPhoneNumber { get; set; }
+        public OrderStatus Status { get; set; }
+    }
+}
---
+++ eShopSolution.Data/Entities/SystemActivitie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Data.Entities
+{
+    public class SystemActivitie
+    {
+        public int Id { get; set; }
+        public string ActionName{ get; set; }
+        public DateTime ActionDate{ get; set; }
+        public int FunctionId { get; set; }
+        public int UserId { get; set; }
+        public string ClientIP { get; set; }
+    }
+}
